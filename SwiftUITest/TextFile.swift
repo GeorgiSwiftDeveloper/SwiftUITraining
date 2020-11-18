235fa6d1a0d76ad73711b73e8f49a4fd81fce4f6
Georgi Malkhasyan <malkhasyangeorge@gmail.com>
2020-11-18 10:10:39 -0800

    SwiftUI Stacks

---
+++ SwiftUITest/TextFile.swift
@@ -0,0 +1,20 @@
+//
+//  TextFile.swift
+//  SwiftUITest
+//
+//  Created by Georgi Malkhasyan on 11/15/20.
+//
+
+import SwiftUI
+
+struct TextFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFile_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFile()
+    }
+}
